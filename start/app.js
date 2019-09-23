7e7482355e0f120eac230f3abea9a993699427ce
Gabriel Batista <svbgabriel@gmail.com>
2019-09-23 20:41:55 -0300

    Configuração da ACL

--- start/app.js
+++ start/app.js
@@ -21,6 +21,7 @@ const providers = [
   '@adonisjs/mail/providers/MailProvider',
   '@adonisjs/framework/providers/ViewProvider',
   '@adonisjs/validator/providers/ValidatorProvider',
+  'adonis-acl/providers/AclProvider',
   'adonis-kue/providers/KueProvider'
 ]
 
@@ -35,6 +36,7 @@ const providers = [
 */
 const aceProviders = [
   '@adonisjs/lucid/providers/MigrationsProvider',
+  'adonis-acl/providers/CommandsProvider',
   'adonis-kue/providers/CommandsProvider'
 ]
 
@@ -50,7 +52,10 @@ const aceProviders = [
 |   { Route: 'Adonis/Src/Route' }
 |
 */
-const aliases = {}
+const aliases = {
+  Role: 'Adonis/Acl/Role',
+  Permission: 'Adonis/Acl/Permission'
+}
 
 /*
 |--------------------------------------------------------------------------
